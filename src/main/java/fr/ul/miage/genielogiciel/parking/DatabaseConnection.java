0ceea1cd26093f58bb54895e5dccf0ec048b951e
Nimarcs <moimonautremoi@gmail.com>
2024-05-22 16:40:03 +0200

    [ADD] change of the charging station (for exemple in case of an nplanned extention of time)

--- src/main/java/fr/ul/miage/genielogiciel/parking/DatabaseConnection.java
+++ src/main/java/fr/ul/miage/genielogiciel/parking/DatabaseConnection.java
@@ -8,7 +8,8 @@ public interface DatabaseConnection {
     /**
      * Give back the reservation of the client at the time given
      * May give back null if no reservation was made at that time
-     * @param client client that made the reservation
+     *
+     * @param client   client that made the reservation
      * @param dateTime time for which the reservation is made
      * @return reservation at that time or null
      */
@@ -17,22 +18,33 @@ public interface DatabaseConnection {
     /**
      * Give back the reservation of the ChargingStation at that time
      * May give back null if the charging station is free at that time
+     *
      * @param chargingStation charging station that was supposedly reserved
-     * @param dateTime time at which the charging station was reserved
+     * @param dateTime        time at which the charging station was reserved
      * @return reservation that booked the charging station or null
      */
     Reservation getReservation(ChargingStation chargingStation, LocalDateTime dateTime);
 
     /**
      * Client linked to the reservation
+     *
      * @param reservation reservation to which the client is linked
      * @return Client liked to the reservation
      */
     Client getClient(Reservation reservation);
 
     /**
+     * Change the charging station of a reservation to the new one
+     *
+     * @param reservation        Reservation to change
+     * @param newChargingStation new charging station that will replace the old one
+     */
+    void changeChargingStation(Reservation reservation, ChargingStation newChargingStation);
+
+    /**
      * Free the charging Station of it's reservation
      * The reservation cost doesn't change
+     *
      * @param chargingStation charging station to free up
      */
     void freeChargingStation(ChargingStation chargingStation);
@@ -40,6 +52,7 @@ public interface DatabaseConnection {
     /**
      * Update the reservation with new data
      * May have problems with synchronicity i need to look into it
+     *
      * @param reservation reservation to update
      */
     void updateReservation(Reservation reservation);
@@ -47,15 +60,17 @@ public interface DatabaseConnection {
     /**
      * Get ALL the reservation of a client in a time frame
      * If the reservation end in the time frame it is counted as valid no matter when it started
+     *
      * @param client client that made the reservation
-     * @param begin beginning of the time frame
-     * @param end end of the time frame
+     * @param begin  beginning of the time frame
+     * @param end    end of the time frame
      * @return List of all the reservation made
      */
     List<Reservation> getReservationsOfClient(Client client, LocalDateTime begin, LocalDateTime end);
 
     /**
      * Get ALL the available station currently
+     *
      * @return List of all currently available charging station
      */
     List<ChargingStation> getAvailiblesChargingStations();
@@ -63,14 +78,16 @@ public interface DatabaseConnection {
     /**
      * Get all client with reservation inside the timeframe
      * If the reservation end in the time frame it is counted as valid no matter when it started
+     *
      * @param begin beginning of the time frame
-     * @param end end of the time frame
+     * @param end   end of the time frame
      * @return List of all active client
      */
     List<Client> getActiveClient(LocalDateTime begin, LocalDateTime end);
 
     /**
      * Get the charging station from the database
+     *
      * @param chargingStation old charging station to obtain again from the database
      * @return charging station from the database
      */
@@ -78,6 +95,7 @@ public interface DatabaseConnection {
 
     /**
      * Get the reservation from the database
+     *
      * @param reservation old reservation to obtain again from the database
      * @return reservation from the database
      */
@@ -85,12 +103,11 @@ public interface DatabaseConnection {
 
     /**
      * Get the client from the database
+     *
      * @param client old client to obtain again from the database
      * @return client from the database
      */
     Client getClient(Client client);
-
-
 
 
 }
