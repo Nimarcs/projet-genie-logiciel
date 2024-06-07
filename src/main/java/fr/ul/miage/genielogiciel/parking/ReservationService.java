9997944838b1e30ddd95a685e8bd898a5d364c9e
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2024-06-07 22:46:51 +0200

    [FIX] Simplify ReservationService

--- src/main/java/fr/ul/miage/genielogiciel/parking/ReservationService.java
+++ src/main/java/fr/ul/miage/genielogiciel/parking/ReservationService.java
@@ -8,83 +8,50 @@ public class ReservationService {
 
     private final ReservationManager reservationManager = new ReservationManager();
 
+    /**
+     * Create a reservation for a charging station
+     * @param input input of the user
+     * @param chargingStations charging station to reserve
+     * @param clients list of current clients
+     */
     public void reserveChargingStation(Scanner input, ChargingStationList chargingStations, ClientList clients) {
-        LocalDateTime currentTime = LocalDateTime.now();
-        LocalDateTime endTime;
         List<ChargingStation> availableStations = chargingStations.findAvailableStations();
 
-        if (!availableStations.isEmpty()) {
-            System.out.println("Available station found: ");
-
-            int i = 1;
-            int selection = -1;
-
-            for (ChargingStation station : availableStations) {
-                System.out.println(i + ". ID: " + station.getIdStation());
-                i++;
-            }
-
-            System.out.print("Please enter the ID of the station you want to reserve: ");
-            selection = input.nextInt();
-            input.nextLine(); // clear the newline character
-
-            ChargingStation selectedStation = chargingStations.findChargingStation(selection);
-
-            if (selectedStation != null && selectedStation.getDisponible()) {
-                System.out.print("Please enter your license number: ");
-                String licenseNumber = input.nextLine();
-
-                Client client = clients.findClientByLicense(licenseNumber);
-
-                if (client != null) {
-                    selection = -1;
-                    System.out.println("Choose what to enter: ");
-                    System.out.println("1. Expected duration ");
-                    System.out.println("2. Departure time ");
-
-                    selection = checkInputMenu(input, 3);
-
-                    switch (selection) {
-                        case 1:
-                            System.out.println("Enter expected duration: ");
-                            int duration = input.nextInt();
-                            endTime = currentTime.plusHours(duration);
-                            break;
-                        case 2:
-                            System.out.println("Enter departure time: ");
-                            int hour = input.nextInt();
-                            endTime = currentTime.withHour(hour);
-                            break;
-                        default:
-                            endTime = currentTime.plusHours(1);
-                    }
-
-                    Reservation reservation = new Reservation(client, LocalDateTime.now(), endTime);
-                    reservationManager.addReservation(selectedStation, reservation);
-                } else {
-                    System.out.print("License number not recognized. Please enter your mobile number: ");
-                    String mobileNumber = input.nextLine();
-
-                    Client client2 = clients.findByMobilePhone(mobileNumber);
-
-                    if (client2 != null) {
-                        System.out.print("Please enter the expected charging duration (in hours): ");
-                        int duration = input.nextInt();
-
-                        System.out.println("Temporary reservation made for mobile number " + mobileNumber + " with duration " + duration + " hours.");
-                        Reservation reservation = new Reservation(client2, currentTime, currentTime.plusHours(duration));
-                        reservationManager.addReservation(selectedStation, reservation);
-                    } else {
-                        System.out.println("We didn't find the account associated with this number. Please create a new one: ");
-                        new CommandLine().run(input, client2, clients, chargingStations);
-                    }
-                }
-            } else {
-                System.out.println("Selected station is not available.");
-            }
-        } else {
+        if (availableStations.isEmpty()) {
             System.out.println("No available stations at the moment. Please try again later.");
+            return;
         }
+
+        System.out.println("Available station found: ");
+
+        int i = 1;
+        int selection;
+
+        for (ChargingStation station : availableStations) {
+            System.out.println(i + ". ID: " + station.getIdStation());
+            i++;
+        }
+
+        System.out.print("Please enter the ID of the station you want to reserve: ");
+        selection = input.nextInt();
+        input.nextLine(); // clear the newline character
+
+        ChargingStation selectedStation = chargingStations.findChargingStation(selection);
+
+        if (!(selectedStation != null && selectedStation.getDisponible())) {
+            System.out.println("Selected station is not available.");
+            return;
+        }
+            System.out.print("Please enter your license number: ");
+            String licenseNumber = input.nextLine();
+
+            Client client = clients.findClientByLicense(licenseNumber);
+
+            if (client != null) {
+                manageClientAndReserve(input, selectedStation, client);
+            } else {
+                findClientAndReserve(input, chargingStations, clients, selectedStation);
+            }
     }
 
     public void handleLateArrival(Scanner input, ChargingStationList chargingStations, ClientList clients) {
@@ -109,7 +76,7 @@ public class ReservationService {
 
 
     private int checkInputMenu(Scanner input, int maxPointsMenu) {
-        int selection = -1;
+        int selection;
 
         while (true) {
             System.out.print("Enter the number of your choice: ");
@@ -129,4 +96,64 @@ public class ReservationService {
 
         return selection;
     }
+
+    /**
+     * Find the client and reserve the charging station
+     * @param input input of the user
+     * @param chargingStations list of all the charging stations
+     * @param clients list of all the clients
+     * @param selectedStation charging station selected by the user
+     */
+    private void findClientAndReserve(Scanner input, ChargingStationList chargingStations, ClientList clients, ChargingStation selectedStation) {
+        System.out.print("License number not recognized. Please enter your mobile number: ");
+        String mobileNumber = input.nextLine();
+
+        Client client2 = clients.findByMobilePhone(mobileNumber);
+
+        if (client2 != null) {
+            System.out.print("Please enter the expected charging duration (in hours): ");
+            int duration = input.nextInt();
+
+            System.out.println("Temporary reservation made for mobile number " + mobileNumber + " with duration " + duration + " hours.");
+            Reservation reservation = new Reservation(client2, LocalDateTime.now(), LocalDateTime.now().plusHours(duration));
+            reservationManager.addReservation(selectedStation, reservation);
+        } else {
+            System.out.println("We didn't find the account associated with this number. Please create a new one: ");
+            new CommandLine().run(input, client2, clients, chargingStations);
+        }
+    }
+
+    /**
+     * Manage a client that want to reserve a charging station
+     * @param input input of the user
+     * @param selectedStation charging station selected by the user
+     * @param client current client
+     */
+    private void manageClientAndReserve(Scanner input, ChargingStation selectedStation, Client client) {
+        int selection;
+        LocalDateTime endTime;
+        System.out.println("Choose what to enter: ");
+        System.out.println("1. Expected duration ");
+        System.out.println("2. Departure time ");
+
+        selection = checkInputMenu(input, 3);
+
+        switch (selection) {
+            case 1 -> {
+                System.out.println("Enter expected duration: ");
+                int duration = input.nextInt();
+                endTime = LocalDateTime.now().plusHours(duration);
+            }
+            case 2 -> {
+                System.out.println("Enter departure time: ");
+                int hour = input.nextInt();
+                endTime = LocalDateTime.now().withHour(hour);
+            }
+            default -> endTime = LocalDateTime.now().plusHours(1);
+        }
+
+        Reservation reservation = new Reservation(client, LocalDateTime.now(), endTime);
+        reservationManager.addReservation(selectedStation, reservation);
+    }
+
 }
