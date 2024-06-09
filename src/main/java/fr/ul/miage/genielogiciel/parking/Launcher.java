694a345160972402a76a61e19d942bacf0fcd6c0
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2024-06-09 10:11:12 +0200

    [ADD] Adapt the launcher to the creation of the facade and the change in commandLine

--- src/main/java/fr/ul/miage/genielogiciel/parking/Launcher.java
+++ src/main/java/fr/ul/miage/genielogiciel/parking/Launcher.java
@@ -1,6 +1,8 @@
 package fr.ul.miage.genielogiciel.parking;
 
 
+import fr.ul.miage.genielogiciel.parking.commandLine.CommandLine;
+
 import java.util.Scanner;
 
 public class Launcher {
@@ -15,29 +17,11 @@ public class Launcher {
      * @param args command line arguments (not used)
      */
     public static void main(String[] args) {
-
-        // for testing
-        ClientList clients = new ClientList();
-
-        Client client1 = new Client();
-        Client client2 = new Client("Lara", "Mara", "39 rue Paris, 54000 Nancy", "+33785546942", "test@gmail.com", "00012342", "LICENSE123", "test", "test2test");
-
-
-        clients.addClient(client1);
-        clients.addClient(client2);
-
-        ChargingStationList chargingStations = new ChargingStationList();
-
-        chargingStations.addStation(new ChargingStation(123, true));
-        chargingStations.addStation(new ChargingStation(456, true));
-        chargingStations.addStation(new ChargingStation(789, false));
-        chargingStations.addStation(new ChargingStation(12, false));
-
-        Scanner scanner = new Scanner(System.in);
-
+        FacadeInterface facadeInterface = new FacadeInterface();
+        facadeInterface.initializeParking();
 
         CommandLine commandLine = new CommandLine();
-        commandLine.run(scanner, client2, clients, chargingStations);
+        commandLine.run(facadeInterface);
     }
 
 
